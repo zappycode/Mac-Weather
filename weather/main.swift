43bc53d60bc1f82ab739da17c0ee2964881c49af
Nick Walter <nick@zappycode.com>
2016-10-27 18:31:50 -0600

    Initial Commit

---
+++ weather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  weather
+//
+//  Created by Nick Walter on 10/27/16.
+//  Copyright © 2016 Zappy Code. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
